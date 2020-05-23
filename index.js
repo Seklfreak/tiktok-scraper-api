625408b5ed46ad073b2769c63cddce330d4c2397
Sebastian Winkler <sebastian@winkler.dev>
2020-05-23 19:03:28 +0200

    adds basic RSS feed option

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const TikTokScraper = require('tiktok-scraper');
+const RSS = require('rss');
+
 const app = express();
 
 app.listen(3000, () => {
@@ -8,9 +10,43 @@ app.listen(3000, () => {
 
 app.get("/users/:username", async (req, res) => {
     try {
-        const posts = await TikTokScraper.user(req.params.username, { number: 10 });
+        const posts = await TikTokScraper.user(req.params.username, {number: 10});
         res.json(posts);
     } catch (error) {
         res.send(error);
     }
+});
+
+app.get("/users/:username/feed", async (req, res) => {
+    let posts;
+    try {
+        posts = await TikTokScraper.user(req.params.username, {number: 10});
+    } catch (error) {
+        res.send(error);
+        return;
+    }
+
+    let feed = new RSS({
+        title: 'TikTok feed for @' + req.params.username,
+        generator: 'https://github.com/Seklfreak/tiktok-scraper-api',
+        feed_url: req.protocol + '://' + req.hostname + req.url,
+        site_url: 'https://www.tiktok.com/@' + req.params.username
+    });
+
+    posts.collector.forEach((item) => {
+        feed.item({
+            title: 'New TikTok post by @' + item.authorMeta.name,
+            description: item.text,
+            url: 'https://www.tiktok.com/@' + item.authorMeta.name + '/video/' + item.id,
+            guid: item.authorMeta.name + '_' + item.id,
+            author: '@' + item.authorMeta.name,
+            enclosure: {
+                'url': item.imageUrl,
+                'type': 'image/jpeg'
+            }
+        })
+    });
+
+    res.header('Content-Type', 'application/xml');
+    res.send(feed.xml({indent: true}));
 });
